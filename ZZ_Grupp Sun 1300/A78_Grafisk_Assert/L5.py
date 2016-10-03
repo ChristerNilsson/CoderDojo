0c6a2906e0fa159e0ff72b02e9b3e656bcd5e844
ChristerNilsson <janchrister.nilsson@gmail.com>
2016-10-03 17:39:53 +0200

    x

--- ZZ_Grupp Sun 1300/A78_Grafisk_Assert/L5.py
+++ ZZ_Grupp Sun 1300/A78_Grafisk_Assert/L5.py
@@ -4,21 +4,49 @@ def lektion5(ass):
 
     with ass.check("greenEllipse"): 
         if ass.errors(): return
-
+        fc(0,1,0)
+        ellipse(120,60,60,40)
 
     with ass.check("greenRect"): 
         if ass.errors(): return
-
+        fc(0,1,0)
+        rect(60,80, 40,50)
 
     with ass.check("redRect"):
         if ass.errors(): return
+        fc(1, 0, 0)
+        rect(80, 70, 40, 100)
 
 
     with ass.check("cross"):
         if ass.errors(): return
+        fc(1,0,0)
+        sc()
+        rect(115,40,10,100)
+        rect(85,70,70,10)
 
                                         
     with ass.check("squareHole"):
         if ass.errors(): return
+        fc(0,1,1)
+        sc(0,1,1)
+        rect(60,60, 80,20)
+        fc(0,1,1)
+        sc(0,1,1)
+        rect(60,80, 20,40)
+        fc(0,1,1)
+        sc(0,1,1)
+        rect(60,120, 80,20)
+        fc(0,1,1)
+        sc(0,1,1)
+        rect(120,80, 20,40)
+        fc()
+        sc(1,0,0)
+        sw(3)
+        rect(60,60, 80,80)
+        fc()
+        sc(1,0,0)
+        sw(3)
+        rect(80,80, 40,40)
 
                                 
--- ZZ_Grupp Tue 1800/A78_Grafisk_Assert/L4.py
+++ ZZ_Grupp Tue 1800/A78_Grafisk_Assert/L4.py
@@ -4,39 +4,22 @@ def lektion4(ass):
 
     with ass.check("whiteCircle"): 
         if ass.errors(): return
-        fc(1)
-        circle(60,80,30)
 
 
     with ass.check("whiteEmptyCircle"): 
         if ass.errors(): return
-        fc()
-        sc(1)
-        sw(2)
-        circle(70, 90, 40)
+
                 
     with ass.check("twoDiscsA"):
         if ass.errors(): return
-        fc(1,0,0)
-        circle(80,100,40)
-        fc(0,1,0)
-        circle(100,120,50)
-        
-        
-        
 
 
-    #with ass.check("twoDiscsB"):
-        #if ass.errors(): return
+    with ass.check("twoDiscsB"):
+        if ass.errors(): return
 
                 
     with ass.check("twoDiscsC"):
         if ass.errors(): return
-        sc(1)
-        fc(1, 0, 0)
-        circle(80, 100, 40)
-        fc(0, 1, 0, 0.5)
-        circle(120, 100, 50)
 
                 
     with ass.check("twoDiscsD"):
